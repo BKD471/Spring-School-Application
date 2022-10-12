e5161f87f3173cf01e755e8eac78b60a39994e83
BKD471 <bhaskarkumardas9@gmail.com>
2022-10-13 01:36:07 +0530

    Added Course page

---
+++ src/main/java/com/example/school/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.example.school.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/courses").setViewName("courses");
+    }
+}
