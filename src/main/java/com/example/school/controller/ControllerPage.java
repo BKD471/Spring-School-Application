58c51b973395d49804c6c5b7a54c6f33c73d64f8
BKD471 <bhaskarkumardas9@gmail.com>
2022-10-12 14:57:28 +0530

    setting up the front page

--- src/main/java/com/example/school/controller/ControllerPage.java
+++ src/main/java/com/example/school/controller/ControllerPage.java
@@ -6,18 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ControllerPage {
-    @RequestMapping(value={"", "/", "home"})
-    public String displayWelcomePage(Model model) {
-        model.addAttribute("username", "Phoenix");
-        return "Welcome.html";
-    }
-    @RequestMapping("/index")
-    public String displayIndexPage() {
-        return "Index.html";
-    }
+//    @RequestMapping(value={"", "/", "home"})
+//    public String displayWelcomePage(Model model) {
+//        model.addAttribute("username", "Phoenix");
+//        return "home.html";
+//    }
+//    @RequestMapping("/index")
+//    public String displayIndexPage() {
+//        return "Index.html";
+//    }
+//
+//    @RequestMapping("/login")
+//    public String displayLoginPage(){
+//        return "Login.html";
+//    }
 
-    @RequestMapping("/login")
-    public String displayLoginPage(){
-        return "Login.html";
+    @RequestMapping(value={"", "/", "home"})
+    public String displayHomePage() {
+        return "home.html";
     }
 }
