a78105374be693089609b523ac283bbdd79b2dc9
BKD471 <bhaskarkumardas9@gmail.com>
2022-10-23 02:18:32 +0530

    Day 10

---
+++ src/main/java/com/example/school/config/ProjectSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.school.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig  {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//      http.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
+        http.authorizeRequests()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
+      return http.build();
+    }
+}
