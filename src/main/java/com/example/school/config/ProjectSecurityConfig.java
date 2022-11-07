8afb874d0fc8b4c9fe77ea4a22289db3d135185a
BKD471 <bhaskarkumardas9@gmail.com>
2022-11-07 20:39:43 +0530

    Adding CSRF

--- src/main/java/com/example/school/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/school/config/ProjectSecurityConfig.java
@@ -2,23 +2,50 @@ package com.example.school.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectSecurityConfig  {
+public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//      http.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
-        http.authorizeRequests()
+        http.csrf().ignoringAntMatchers("/saveMsg").and()
+                .authorizeRequests()
+                .mvcMatchers("/dashboard").authenticated()
                 .mvcMatchers("/home").permitAll()
                 .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
                 .mvcMatchers("/saveMsg").permitAll()
                 .mvcMatchers("/courses").permitAll()
                 .mvcMatchers("/about").permitAll()
-                .and().formLogin().and().httpBasic();
-      return http.build();
+                .mvcMatchers("/login").permitAll()
+                .and().formLogin().loginPage("/login")
+                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll().
+                and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll().
+        and().httpBasic();
+        return http.build();
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
