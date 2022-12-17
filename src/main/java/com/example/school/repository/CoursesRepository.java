dd148682a141ecab243492b28a012d418e6f4f57
BKD471 <bhaskarkumardas9@gmail.com>
2022-12-17 20:02:57 +0530

    Adding Security to REST endpoints exposed by spring data rest

--- src/main/java/com/example/school/repository/CoursesRepository.java
+++ src/main/java/com/example/school/repository/CoursesRepository.java
@@ -2,10 +2,12 @@ package com.example.school.repository;
 
 import com.example.school.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@RepositoryRestResource(path="courses")
 @Repository
 public interface CoursesRepository extends JpaRepository<Courses,Integer> {
     List<Courses> findByOrderByNameDesc();
--- src/main/java/com/example/school/repository/RolesRepository.java
+++ src/main/java/com/example/school/repository/RolesRepository.java
@@ -2,7 +2,9 @@ package com.example.school.repository;
 
 import com.example.school.model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface RolesRepository extends JpaRepository<Roles,Integer> {
     Roles findByRoleName(String role);
 }
